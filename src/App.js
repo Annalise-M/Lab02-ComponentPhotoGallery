4f0d41734eacbb1a8f15b5b7b8061e61ea0e3571
Annalise Murphy <nali247@gmail.com>
2020-04-28 21:27:57 -0700

    more added to App.js and ImageItem.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import './App.css';
 import creatureData from './data.js';
 import ImageItem from './ImageItem.js';
-
+import './App.css';
 
 // import default from './header.js';
 
@@ -31,10 +30,25 @@ class App extends React.Component {
             <option value="" defaultValue>
               All Types
             </option>
-            <option value="">HornedAnimal</option>
-            <option value="">HornedAnimal</option>
-            <option value="">HornedAnimal</option>
-            <option value="">HornedAnimal</option>
+            {/* Start inputing dynamic data */}
+            <option value="UniWhal">UniWhal</option>
+            <option value="rhino">Rhino Family</option>
+            <option value="unicorn">Unicorn Head</option>
+            <option value="unilego">UniLego</option>
+            <option value="narwhal">Basically a unicorn</option>
+            <option value="narwhal">#truth</option>
+            <option value="rhino">Baby Rhino</option>
+            <option value="triceratops">Cera</option>
+            <option value="narwhal">Narwhal costume</option>
+            <option value="rhino">Rhino costume</option>
+            <option value="unicorn">Believe</option>
+            <option value="markhor">Markhor</option>
+            <option value="markhor">Baby markhor</option>
+            <option value="mouflon">Baby mouflon</option>
+            <option value="chameleon">Happy Jackson's Chameleon</option>
+            <option value="chameleon">Serious Jackson's Chameleon</option>
+            <option value="lizard">Horned Lizard</option>
+            <option value="dragon">Smaug</option>
           </section>
 
         </section> 
--- src/ImageItem.js
+++ src/ImageItem.js
@@ -3,22 +3,23 @@ import React, { Component } from "react";
 export default class ImageItem extends Component {
     render() {
         return (
-            <div>
+            
                 <li>
-                    <div>
+                    <div className="image-container">
                         <h2>{this.props.creature.title}Creature Name</h2>
                         <p className="horns">Horn Amount = {this.props.creature.horns}</p>
                     </div>
 
-                    <div className="image">
+                    <div className="info-container">
                         <img
                             alt={this.props.creature.url}
                             src={this.props.creature.url}>
                         </img>
                     </div>
-                    <p className="description">{this.props.creature.description}</p>
+                    <p className="creature-description">{this.props.creature.description}</p>
+                    <p className="creature-keyword">{this.props.creature.keyword}</p>
+                    <p className="creature-horns">{this.props.creature.description}</p>
                 </li>
-            </div>
         )
     }
 }
